c7712e858112d505bf0ceb4fc61e6d674b56c3c1
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 20:46:41 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,8 +2,8 @@
 
 from main import check_python_version_and_os
 
-required_version = "3.7, 3.8, 3.9, 3.11"
-required_os = "Linux, Windows"
+required_version = ["3.7", "3.8", "3.9", "3.11"]
+required_os = ["Linux", "Windows"]
 
 python_version_meets_requirement, os_name_matches_requirement = check_python_version_and_os(required_version, required_os)
 
