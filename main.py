fa8f0606b776975a45b58eb66adefdbffa02153a
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 20:49:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def check_python_version_and_os(required_version, required_os):
     python_version = re.search(r"\d+\.\d+", sys.version).group()
     os_name = os.getenv("RUNNER_OS")
 
-    python_version_meets_requirement = python_version >= required_version
+    python_version_meets_requirement = python_version == required_version
     os_name_matches_requirement = os_name == required_os
 
     return python_version_meets_requirement, os_name_matches_requirement
