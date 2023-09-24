83e86a4201191c4a0bc3ad06e1292ad05db2f2a7
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 20:45:56 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@
 
 from main import check_python_version_and_os
 
-required_version = "3.6"
+required_version = "3.7, 3.8, 3.9, 3.11"
 required_os = "Linux"
 
 python_version_meets_requirement, os_name_matches_requirement = check_python_version_and_os(required_version, required_os)
