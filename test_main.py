3ef9660c31d029afef20df337752e930f3ce2a0f
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 21:14:25 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,15 +1,22 @@
-#this is for testing functions in main.py
-
 from main import check_python_version_and_os
 
-required_version = ["3.7", "3.8", "3.9", "3.11"]
-required_os = ["Linux", "Windows"]
-
-python_version_meets_requirement, os_name_matches_requirement = check_python_version_and_os(required_version, required_os)
-
-# Assertions for testing
-assert python_version_meets_requirement, f"Python version {required_version} or higher is not satisfied."
-if os_name_matches_requirement is not None: 
-  assert os_name_matches_requirement, f"OS {required_os} is not satisfied."
-
-print("Both requirements are satisfied.")
+def test_check_python_version_and_os():
+  required_versions = ["3.7", "3.8", "3.9", "3.11"]
+  required_oses = ["Linux", "Windows"]
+  
+  all_requirements_met = True
+  
+  for version in required_versions:
+      for os_name in required_oses:
+          python_version_meets_requirement, os_name_matches_requirement = check_python_version_and_os(version, os_name)
+  
+          # Assertions for testing
+          assert python_version_meets_requirement, f"Python version {version} or higher is not satisfied on {os_name}."
+          assert os_name_matches_requirement, f"OS {os_name} is not satisfied for Python version {version}."
+  
+          all_requirements_met = all_requirements_met and python_version_meets_requirement and os_name_matches_requirement
+  
+  if all_requirements_met:
+      print("All requirements are satisfied.")
+  else:
+      print("Some requirements are not satisfied.")
