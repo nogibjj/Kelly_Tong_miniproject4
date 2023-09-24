7ac35c6f9c12c316bc4fcb251f1e5fccee2ed7ae
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 21:31:52 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,9 +4,4 @@ def test_check_python_version_and_os():
     required_versions = ["3.7", "3.8", "3.9", "3.11"]
     required_oses = ["Linux", "Windows"]
 
-    for version in required_versions:
-        for os_name in required_oses:
-            python_version_meets_requirement, os_name_matches_requirement = check_python_version_and_os([version], [os_name])
-
-            assert python_version_meets_requirement, f"Python version {version} or higher is not satisfied on {os_name}."
-            assert os_name_matches_requirement, f"OS {os_name} is not satisfied for Python version {version}."
+    assert check_python_version_and_os(required_versions, required_oses) == True
