7a788a98ebc4275becf5cf6e1d8914e217211250
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 21:25:13 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,8 +4,6 @@ def test_check_python_version_and_os():
   required_versions = ["3.7", "3.8", "3.9", "3.11"]
   required_oses = ["Linux", "Windows"]
   
-  all_requirements_met = True
-  
   for version in required_versions:
       for os_name in required_oses:
           python_version_meets_requirement, os_name_matches_requirement = check_python_version_and_os(version, os_name)
@@ -13,10 +11,4 @@ def test_check_python_version_and_os():
           # Assertions for testing
           assert python_version_meets_requirement == True
           assert os_name_matches_requirement == True
-  
-          all_requirements_met = all_requirements_met and python_version_meets_requirement and os_name_matches_requirement
-  
-  if all_requirements_met:
-      print("All requirements are satisfied.")
-  else:
-      print("Some requirements are not satisfied.")
+
