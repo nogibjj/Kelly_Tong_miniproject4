bab048aab7b804701a7b470e815414b1f4ab3c6f
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-23 20:38:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def subtraction(x,y):
-    result = 0
-    result = x - y
-    return result
+import os
+import sys
+import re
+
+def check_python_version_and_os(required_version, required_os):
+    try:
+        python_version = re.search(r"\d+\.\d+", sys.version).group()
+        os_name = os.getenv("RUNNER_OS")
+
+        python_version_meets_requirement = python_version >= required_version
+        os_name_matches_requirement = os_name == required_os
+
+        return python_version_meets_requirement, os_name_matches_requirement
+    except Exception as e:
+        return False, False
